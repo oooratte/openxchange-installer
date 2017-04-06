c33ef79fd72065479ee990f25c9f89d870b23b3b
Bart Verwilst <bart.verwilst@sentia.com>
2017-04-06 17:43:15 +0200

    working postfix and dovecot

---
+++ roles/percona-server/files/schema.sql
@@ -0,0 +1,25 @@
+CREATE TABLE `domains` (
+	  `id` int(11) unsigned NOT NULL auto_increment,
+	  `name` varchar(128) NOT NULL,
+	  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `users` (
+	  `id` int(11) unsigned NOT NULL auto_increment,
+	  `domain_id` int(11) unsigned NOT NULL,
+	  `email` varchar(128) NOT NULL,
+	  `password` varchar(128) NOT NULL,
+	  `active` tinyint(1) unsigned NOT NULL DEFAULT 1,
+	  PRIMARY KEY (`id`),
+	  UNIQUE KEY `email` (`email`),
+	  FOREIGN KEY (domain_id) REFERENCES domains(id) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `aliases` (
+	  `id` int(11) unsigned NOT NULL auto_increment,
+	  `domain_id` int(11) unsigned NOT NULL,
+	  `source` varchar(128) NOT NULL,
+	  `destination` varchar(128) NOT NULL,
+	  PRIMARY KEY (`id`),
+	  FOREIGN KEY (domain_id) REFERENCES domains(id) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
