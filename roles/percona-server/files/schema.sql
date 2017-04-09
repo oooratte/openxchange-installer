f6a3f2b86b3320e383e9b3c5040507e7225d0a7a
Bart Verwilst <bart@verwilst.be>
2017-04-09 23:42:19 +0200

    no need for vmail.users table

--- roles/percona-server/files/schema.sql
+++ roles/percona-server/files/schema.sql
@@ -4,16 +4,6 @@ CREATE TABLE `domains` (
 	  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `users` (
-	  `id` int(11) unsigned NOT NULL auto_increment,
-	  `domain_id` int(11) unsigned NOT NULL,
-	  `email` varchar(128) NOT NULL,
-	  `active` tinyint(1) unsigned NOT NULL DEFAULT 1,
-	  PRIMARY KEY (`id`),
-	  UNIQUE KEY `email` (`email`),
-	  FOREIGN KEY (domain_id) REFERENCES domains(id) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 CREATE TABLE `aliases` (
 	  `id` int(11) unsigned NOT NULL auto_increment,
 	  `domain_id` int(11) unsigned NOT NULL,
